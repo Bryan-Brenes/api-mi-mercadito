cf29a4e22a12ff511a32e7c3c761b76f5d4f5360
Bryan Brenes <bryanbrenes1@gmail.com>
2020-01-19 07:49:47 -0600

    prueba BD

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
-const postgresql = require('pg');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const { Pool, Client } = require('pg')
 
+const connectionString = 'postgres://pathrcjxbkzlmi:398913e00f91c50e77d5fb249e0ce66c2df9fde58369aad3e68fa97b4e5f3c5f@ec2-174-129-33-13.compute-1.amazonaws.com:5432/d3g06p50108qd3?ssl=true'
+const pool = new Pool({
+    connectionString: connectionString
+})
 const app = express();
 // app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send("hola mundo!")
+    pool.query(`select * from datos`, (err, res2) => {
+
+        if (err) {
+            res.send("error: " + err)
+        } else {
+            res.send(res2.rows);
+        }
+    });
 })
 
 
