f52d71cf3590271f174fbf209cf5106fb8af8073
Bryan Brenes <bryanbrenes1@gmail.com>
2020-01-20 16:40:03 -0600

    cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool, Client } = require('pg')
+const { Pool, Client } = require('pg');
+const cors = require('cors');
 
 const connectionString = 'postgres://pathrcjxbkzlmi:398913e00f91c50e77d5fb249e0ce66c2df9fde58369aad3e68fa97b4e5f3c5f@ec2-174-129-33-13.compute-1.amazonaws.com:5432/d3g06p50108qd3?ssl=true'
 const pool = new Pool({
     connectionString: connectionString
 })
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 
 /**
