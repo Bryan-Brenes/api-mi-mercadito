c6b31ca940258fff65f379a58e6fabb2cef23c4b
Bryan Brenes <bryanbrenes1@gmail.com>
2020-01-22 04:30:17 -0600

    solicitudes pendientes get

--- index.js
+++ index.js
@@ -178,8 +178,8 @@ app.get('/productos', (req, res) => {
     });
 })
 
-app.get('/invertario/feria/:idC', (req, res) => {
-    var idC = req.params.idC;
+app.get('/invertario/feria/:idComerciante', (req, res) => {
+    var idC = req.params.idComerciante;
     pool.query(`select idproducto, nombre, cantidad, montokg, montounidad from productosporferia inner join productos on productosporferia.idproducto = productos.id where productosporferia.idcomerciante = '${idC}'`, (err, res2) => {
         if (err) {
             res.send("error: " + err)
